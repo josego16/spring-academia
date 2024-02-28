1529b922fd623c7dddfb910f54f7f98feb3f1d6f
josema <josemagomez53@gmail.com>
2024-02-28 21:57:16 +0100

    Initial commit

---
+++ src/main/java/com/acceso/springacademia/SpringAcademiaApplication.java
@@ -0,0 +1,13 @@
+package com.acceso.springacademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAcademiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAcademiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acceso/springacademia/SpringAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acceso.springacademia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAcademiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
